e6eb9622bbd44e5ad08a950a11d30cf1017fa7f6
felipetshop <felipesampaio159@live.com>
2024-04-13 17:05:25 -0300

    85%
    Falta ajeitar a mensagem de erro e configurar para mobile.

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
 
-const botaopag2 = document.getElementById(btn2);
-const botaopag1 = document.getElementById(btn1);
-const pagina1 = document.getElementById(card)
-const pagina2 = document.getElementById(card2)
+    const card1 = document.querySelector('.card');
+    const card2 = document.querySelector('.card2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.querySelector('.btn2');
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('emailError');
+  
+    
+    btn1.addEventListener('click', function(event) {
+      event.preventDefault(); // Previne o envio do formulário
+  
+      
+      if (!validateEmail(emailInput.value)) {
+        emailError.style.display = 'flex';
+        return; // Retorna para evitar o envio do formulário
+      }
+  
+      emailError.style.display = 'none';
+  
+      
+      card1.style.display = 'none';
+      card2.style.display = 'block';
+    });
+  
+    
+    btn2.addEventListener('click', function() {
+      
+      card2.style.display = 'none';
+      card1.style.display = 'flex'; // Altera para 'flex' para manter o layout
+    });
+  
+    
+    function validateEmail(email) {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    }
+  });
+  
 
-botaopag2.addEventListener('click',function(){
-    card.style.display = 'none';
-    card2.style.display = 'block';
-});
-
-botaopag1.addEventListener('click',function(){
-    card2.style.display = 'none';
-    card.style.display = 'block';
-
-});
